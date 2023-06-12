0b6953d12f3616b24159f7a231a4cb87f8606156
Caetano <127361836+Kkanzaki@users.noreply.github.com>
2023-06-12 19:23:34 -0400

    Solutions

--- Iniciante/php/1002.php
+++ Iniciante/php/1002.php
@@ -3,7 +3,7 @@
 $π = 3.14159;
 $raio = trim(fgets(STDIN));
 $area = $π * ($raio * $raio);
-$area = number_format($area,4,".","");
+$area = number_format($area, 4, ".", "");
 echo "A=$area\n";
 
 ?>
--- Iniciante/php/1012.php
+++ Iniciante/php/1012.php
@@ -1,28 +1,28 @@
 <?php
 $valores = trim(fgets(STDIN));
-$vlist = explode(" ",$valores);
+$vlist = explode(" ", $valores);
 $a = floatval($vlist[0]);
 $b = floatval($vlist[1]);
 $c = floatval($vlist[2]);
 
-$triangulo = ($a * $c)/2;
-$triangulo = number_format($triangulo,3,".","");
+$triangulo = ($a * $c) / 2;
+$triangulo = number_format($triangulo, 3, ".", "");
 echo "TRIANGULO: $triangulo\n";
 
 $pi = 3.14159;
 $circulo = $pi * ($c * $c);
-$circulo = number_format($circulo,3,".","");
+$circulo = number_format($circulo, 3, ".", "");
 echo "CIRCULO: $circulo\n";
 
-$trapezio = (($a + $b)* $c)/2;
-$trapezio = number_format($trapezio,3,".","");
+$trapezio = (($a + $b) * $c) / 2;
+$trapezio = number_format($trapezio, 3, ".", "");
 echo "TRAPEZIO: $trapezio\n";
 
 $quadrado = $b * $b;
-$quadrado = number_format($quadrado,3,".","");
+$quadrado = number_format($quadrado, 3, ".", "");
 echo "QUADRADO: $quadrado\n";
 
 $retangulo = $a * $b;
-$retangulo = number_format($retangulo,3,".","");
+$retangulo = number_format($retangulo, 3, ".", "");
 echo "RETANGULO: $retangulo\n";
 ?>
